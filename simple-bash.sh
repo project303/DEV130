3ae3f3818fd4be2ad4dd128520e969a8333d92c4
project303 <32200860+project303@users.noreply.github.com>
2021-07-07 23:35:43 +0700

    Update simple-bash.sh

--- simple-bash.sh
+++ simple-bash.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
-# USER_NAME="Bob"
+USER_NAME="Bob"
 # USER_NAME=$(whoami)
-USER_NAME=$1
+# USER_NAME=$1
 
 echo "Apa kabar ${USER_NAME}?"
 
-CURR_DATETIME=$(date +"%Y-%m-%d %H:%m:%S")
-echo "$CURR_DATETIME  Processing file"
+# CURR_DATETIME=$(date +"%Y-%m-%d %H:%m:%S")
+# echo "$CURR_DATETIME  Processing file"
 
+<<'###'
 for i in {1..5}
 do
    echo "1|2|3" > dataset-s0$i.csv
@@ -28,9 +29,10 @@ do
        echo  "3|5|6" >> dataset-s0$i.csv
    fi
 done
+###
 
 
-
+<<'###'
 for i in $(ls dataset-s*.csv)
 do
    line_number=`wc -l ${i} | awk -F' ' '{print $1}'`
@@ -38,3 +40,4 @@ do
 done
 
 echo "$(date +"%Y-%m-%d %H:%m:%S")  Processing done"
+###
